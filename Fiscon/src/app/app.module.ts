2e113a63ad65cbbe399a806f231c14d6536f934c
Madreyv <madreyv@gmail.com>
2022-10-08 20:35:10 -0300

    :feature: criado projeto e adcionado componentes de form do material

---
+++ Fiscon/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { TabelaComponent } from './componentes/tabela/tabela.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent,
+    TabelaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Fiscon/src/app/componentes/formulario/formulario.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+  public formulario!: FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.criarFormulario()
+  }
+
+  criarFormulario() {
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      telefone: ['', Validators.required],
+    })
+  };
+
+}
